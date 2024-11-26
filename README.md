153f5fbf120c64b64877959b674f988894d98b05
Disperseur <disperseur.code@gmail.com>
2024-11-26 18:23:07 +0100

    ajout exemple de fonction d'affichage de commande. ajout du fichier qui donne les principales fonctions

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# Framework_securite_reseau
-Framework du travail de groupe de sécurité des réseaux informatiques en entreprise
+# Notes sécurité des réseaux
+
+
+# Pour voir son ip: 
+``$sudo ifconfig``
+
+# Pour scanner les appareils actifs sur le réseau:
+``$sudo fping -s -g ip_deb ip_fin --alive -q``
+
+# Pour scanner un appareil:
+``$sudo nmap -sS ip_appareil``
+
+# Pour scanner les différents services d'un appareil
+
+## Telnet
+
+``$telnet <ip_addr> <port>``\
+puis hit Esc et Enter
+-> affiche la banniere
+
+## netcat (nc)
+``$nc <ip_addr> <port>``\
+``$GET /index.html``
+
+## nmap (best)
+``$nmap -sV -script=banner <ip_addr>``
+
+
+
+
+
+
--- framework.sh
+++ framework.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-
-# Fonction pour afficher le menu principal
 show_mainMenu() {
     # Efface l'écran et replace le curseur en haut
     tput clear
@@ -22,7 +19,7 @@ show_footprintMenu() {
     tput clear
     tput cup 0 0
     echo "============================="
-    echo "    FRAMEWORK - FOOTPRINT    "
+    echo "    FRAMEWORK - Footprint    "
     echo "============================="
     echo "1. IP Addresses"
     echo "2. whois"
@@ -32,18 +29,32 @@ show_footprintMenu() {
     echo "Choose an option then press ENTER."
 }
 
+
+
+show_scanningNetworksMenu() {
+    # Efface l'écran et replace le curseur en haut
+    tput clear
+    tput cup 0 0
+    echo "====================================="
+    echo "    FRAMEWORK - Scanning networks    "
+    echo "====================================="
+    echo "1. ping"
+    echo "2. fping"
+    echo "3. nmap"
+    echo "4. main menu"
+    echo
+    echo "Choose an option then press ENTER."
+}
+
 # Fonction pour exécuter les choix
 handle_choice_mainMenu() {
     tput clear
     case $1 in
         1)
-            tput cup 0 0
-            echo "Vous avez choisi l'Option 1."
             run_footprintMenu
             ;;
         2)
-            tput cup 0 0
-            echo "Vous avez choisi l'Option 2."
+            run_scanningNetworksMenu
             ;;
         3)
             tput cup 0 0
@@ -68,8 +79,7 @@ handle_choice_mainMenu() {
 handle_choice_footprintMenu() {
     case $1 in
         1)
-            tput cup 0 0
-            echo "Vous avez choisi l'Option footprint 1."
+            command_ipaddress
             ;;
         2)
             tput cup 0 0
@@ -92,6 +102,55 @@ handle_choice_footprintMenu() {
     return 0 # Retourne 0 par défaut si aucune condition spécifique n'est remplie
 }
 
+handle_choice_scanningNetworksMenu() {
+    case $1 in
+        1)
+            tput cup 0 0
+            echo "Vous avez choisi l'Option scanning networks 1."
+            ;;
+        2)
+            tput cup 0 0
+            echo "Vous avez choisi l'Option footprint 2."
+            ;;
+        3)
+            tput cup 0 0
+            echo "Vous avez choisi l'Option footprint 3."
+            ;;
+        4)
+            tput cup 0 0
+            echo "Retour au main menu."
+            return -1 # Retourne -1 à l'appelant
+            ;;
+        *)
+            tput cup 0 0
+            echo "Choix invalide. Veuillez réessayer."
+            ;;
+    esac
+    return 0 # Retourne 0 par défaut si aucune condition spécifique n'est remplie
+}
+
+
+
+
+command_ipaddress() {
+    tput clear
+    tput cup 0 0
+    echo "=================================================="
+    echo "    FRAMEWORK - Scanning networks - IP address    "
+    echo "=================================================="
+    echo
+    echo "Executed command: '$ hostname -I'"
+    echo "Command ouput: "
+    echo $(hostname -I)
+    echo
+    read -p "[ENTER]"
+}
+
+
+
+
+
+
 run_mainMenu() {
     while true; do
         show_mainMenu
@@ -111,6 +170,24 @@ run_footprintMenu() {
         fi
     done
 }
+
+run_scanningNetworksMenu() {
+    while true; do
+        show_scanningNetworksMenu
+        read -p "Entrez votre choix [1-4]: " choice
+        handle_choice_scanningNetworksMenu "$choice"
+        # Capture le code de retour de la fonction
+        if [ $? -eq 255 ]; then # -1 est représenté comme 255 en Bash
+            break
+        fi
+    done
+}
+
+
+
+
+
+
 
 # Boucle principale
 run_mainMenu
